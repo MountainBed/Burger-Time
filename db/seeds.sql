0357e1f16c85f76cf716ed6efecf65f64fb44838
Ross Blair <rblair33@gmail.com>
2017-09-24 10:49:38 -0400

    DB setup with psuedo information.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Butt Burger");
+INSERT INTO burgers (burger_name) VALUES ("Garbage Burger");
+INSERT INTO burgers (burger_name) VALUES ("Hair Burger");
+INSERT INTO burgers (burger_name) VALUES ("Slime Burger");
+INSERT INTO burgers (burger_name) VALUES ("Moldy Banana Burger");
+INSERT INTO burgers (burger_name) VALUES ("Swampy Burger");
