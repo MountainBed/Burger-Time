72604735e743bb8c05660648848edb952427abb2
Ross Blair <rblair33@gmail.com>
2017-09-24 17:01:59 -0400

    burger controller is set up.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,30 @@
 var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+  burger.all(function (data) {
+    var handlebarsObject = {
+      burger: data
+    };
+    res.render("index", handlebarsObject);
+  });
+});
+
+router.post("/", function (req, res) {
+  burger.create("burger_name", req.body.name, function () {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function () {
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
