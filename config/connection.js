dbed706d2a2a69b2bb9f5cedaa1dc98ed0609c45
Ross Blair <rblair33@gmail.com>
2017-09-27 20:21:23 -0400

    Set up to use JAWSDB on Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
 var key = require("./keys.js");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: key.keySQL,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: key.keySQL,
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
